ab9d21bf49e097cd45b6346d06b15533a47e6c2b
Jon Schoning <jonschoning@gmail.com>
2014-03-27 16:24:06 -0500

    adding mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    stylus = require('stylus');
+    stylus = require('stylus'),
+    mongoose = require('mongoose');
 var env = process.env.NODE_ENV =process.env.NODE_ENV || 'development';
 var app = express();
 
@@ -32,3 +33,10 @@ app.get('*', function(req, res) {
 var port = 3030;
 app.listen(port);
 console.log('listening on port ' + port + '...');
+
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('multivision db opened');
+});
