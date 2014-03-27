fb83464d2e3007c122211ba569676f6b4b037cdd
Jon Schoning <jonschoning@gmail.com>
2014-03-27 16:15:28 -0500

    creating the angular application

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.configure(function() {
 });
 
 // routes
+app.get('/partials/:partialPath', function(req, res) {
+  res.render('partials/' + req.params.partialPath);
+});
 app.get('*', function(req, res) {
   res.render('index');
 });
