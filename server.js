da8ce7f0e9b45f431608bdbf8329089c4877b67d
Jon Schoning <jonschoning@gmail.com>
2014-03-28 00:34:12 -0500

    adding the login display

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ db.once('open', function callback() {
 });
 
 // routes
-app.get('/partials/:partialPath', function(req, res) {
-  res.render('partials/' + req.params.partialPath);
+app.get('/partials/*', function(req, res) {
+  res.render('../../public/app/' + req.params);
 });
 app.get('*', function(req, res) {
   res.render('index');
