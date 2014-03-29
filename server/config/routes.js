63989c561d2c0a22850dd282e7c74200d41b6bad
Jon Schoning <jonschoning@gmail.com>
2014-03-28 21:27:42 -0500

    creating the server login code

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,27 @@
+var passport = require('passport');
 
 module.exports = function(app) {
 
   app.get('/partials/*', function(req, res) {
     res.render('../../public/app/' + req.params);
   });
+
+  app.post('/login', function(req, res, next) {
+    var auth = passport.authenticate('local', function(err, user) {
+      if(err) { return next(err); }
+      if(!user) { res.send({success:false});}
+
+      // required because we're using XHR to login
+      req.logIn(user, function(err) {
+        if(err) {return next(err);}
+        res.send({success:true, user: user});
+      });
+
+    });
+    auth(req, res, next);
+  });
+
+
   app.get('*', function(req, res) {
     res.render('index');
   });
