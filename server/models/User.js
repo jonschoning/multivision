1ce3b1c79a84db4c3c58b903c9b6096f6eb98e50
Jon Schoning <jonschoning@gmail.com>
2014-03-29 22:57:28 -0500

    implementing model validation

--- server/models/User.js
+++ server/models/User.js
@@ -3,11 +3,15 @@ var mongoose = require('mongoose'),
     encrypt = require('../utilities/encryption');
 
   var userSchema = mongoose.Schema({
-    firstName: String,
+    firstName: {type:String, required:'{PATH} is required!'},
     lastName: String,
-    userName: String,
-    salt: String,
-    hashed_pwd: String,
+    userName: {
+      type:String, 
+      required:'{PATH} is required!',
+      unique:true
+    },
+    salt: {type:String, required:'{PATH} is required!'},
+    hashed_pwd: {type:String, required:'{PATH} is required!'},
     roles: [String]
   });
 
@@ -26,16 +30,16 @@ var mongoose = require('mongoose'),
         var salt, hash;
 
         salt = encrypt.createSalt();
-        hash = encrypt.hashPwd(salt, 'jsimmons');
-        User.create({firstName:'Javier', lastName:'Simmons', userName:'jsimmons', salt: salt, hashed_pwd: hash, roles: ['admin']});
+        hash = encrypt.hashPwd(salt, 'j@j.com');
+        User.create({firstName:'Javier', lastName:'Simmons', userName:'j@j.com', salt: salt, hashed_pwd: hash, roles: ['admin']});
 
         salt = encrypt.createSalt();
-        hash = encrypt.hashPwd(salt, 'cweaver');
-        User.create({firstName:'Christy', lastName:'Weaver', userName:'cweaver', salt: salt, hashed_pwd: hash, roles: []});
+        hash = encrypt.hashPwd(salt, 'c@c.com');
+        User.create({firstName:'Christy', lastName:'Weaver', userName:'c@c.com', salt: salt, hashed_pwd: hash, roles: []});
    
         salt = encrypt.createSalt();
-        hash = encrypt.hashPwd(salt, 'asteward');
-        User.create({firstName:'Adam', lastName:'Steward', userName:'asteward', salt: salt, hashed_pwd: hash});
+        hash = encrypt.hashPwd(salt, 'a@a.com');
+        User.create({firstName:'Adam', lastName:'Steward', userName:'a@a.com', salt: salt, hashed_pwd: hash});
       }
     });
   }
